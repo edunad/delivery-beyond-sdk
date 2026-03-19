de50becdf7609cc85f2c6d8de63181fb47e04e74
FailCake <4944278+edunad@users.noreply.github.com>
2026-03-19 22:46:37 +0100

    Add monster assets & sdks

--- com.failcake.db.sdk/Scripts/Entities/entity_sdk/entity_sdk_phys_scrap.cs
+++ com.failcake.db.sdk/Scripts/Entities/entity_sdk/entity_sdk_phys_scrap.cs
@@ -1,29 +1,9 @@
-﻿#region
-
-using SaintsField;
-using UnityEngine;
-
-#endregion
-
-namespace HyenaQuest
+﻿namespace HyenaQuest
 {
-    public class entity_sdk_phys_scrap : MonoBehaviour
+    public class entity_sdk_phys_scrap : entity_sdk_replacement
     {
-        #if UNITY_EDITOR
-        [FieldInfoBox("DO NOT EDIT, USED TO MAP TEMPLATES ON THE MAIN GAME", EMessageType.Error)]
-        public Mesh preview;
-
-        public void OnDrawGizmos() {
-            if (!this.preview) return;
-            
-            Gizmos.matrix = Matrix4x4.TRS(this.transform.position, this.transform.rotation, Vector3.one);
-            Gizmos.DrawMesh(this.preview);
+        public new void Awake() {
+            // HANDLED BY entity_template
         }
-
-        public void OnValidate() {
-            if (Application.isPlaying || !this.preview) return;
-            this.name = $"SDK-{this.preview.name}";
-        }
-        #endif
     }
 }
---
+++ com.failcake.db.sdk/Scripts/Entities/entity_sdk/entity_sdk_replacement.cs
@@ -0,0 +1,39 @@
+﻿#region
+
+using SaintsField;
+using UnityEngine;
+
+#endregion
+
+namespace HyenaQuest
+{
+    public class entity_sdk_replacement : MonoBehaviour
+    {
+        #if UNITY_EDITOR
+        [FieldInfoBox("DO NOT EDIT, USED TO MAP TEMPLATES ON THE MAIN GAME", EMessageType.Error)]
+        public Mesh preview;
+        #endif
+
+        public void Awake() {
+            SDK_SETUP.PatchSDKEntity?.Invoke(this.gameObject);
+        }
+
+        #if UNITY_EDITOR
+        private void OnDrawGizmos() {
+            if (!this.preview) return;
+
+            Gizmos.matrix = Matrix4x4.TRS(this.transform.position, this.transform.rotation, Vector3.one);
+            Gizmos.DrawMesh(this.preview);
+        }
+
+        private string GetUniqueID() {
+            return !this.preview ? "unknown" : this.preview.name;
+        }
+
+        private void OnValidate() {
+            if (Application.isPlaying || !this.preview) return;
+            this.name = $"SDK-{this.GetUniqueID()}";
+        }
+        #endif
+    }
+}
