c85d9260fb2072ca581ee32579153aa26d170378
FailCake <4944278+edunad@users.noreply.github.com>
2026-03-20 16:07:55 +0100

    - Add NetworkBehaviour back to entity_sdk_interior_door and entity_sdk_custom_phys_scrap

--- com.failcake.db.sdk/Scripts/Entities/entity_sdk/entity_sdk_custom_phys_scrap.cs
+++ com.failcake.db.sdk/Scripts/Entities/entity_sdk/entity_sdk_custom_phys_scrap.cs
@@ -15,8 +15,9 @@ namespace HyenaQuest
     /// <summary>
     ///     PROXY to entity_phys_scrap
     /// </summary>
-    [DisallowMultipleComponent]
-    public class entity_sdk_custom_phys_scrap : MonoBehaviour
+    
+    [DisallowMultipleComponent, RequireComponent(typeof(NetworkObject), typeof(NetworkTransform)), RequireComponent(typeof(NetworkRigidbody), typeof(Rigidbody))]
+    public class entity_sdk_custom_phys_scrap : NetworkBehaviour
     {
         [LayoutStart("Sounds", ELayout.Background | ELayout.TitleOut)]
         public SoundTypes collisionMaterial = SoundTypes.CUSTOM;
@@ -35,12 +36,73 @@ namespace HyenaQuest
         private bool UseCustomSounds => this.collisionMaterial == SoundTypes.CUSTOM;
 
         #if UNITY_EDITOR
+         /// <summary>
+        /// USED TO MIMIC entity_door
+        /// </summary>
+        
+        private NetworkTransform _networkTransform;
+        private Rigidbody _rigidbody;
+        
         public void OnValidate() {
             if (Application.isPlaying) return;
             
             string newName = "entity_sdk_custom_phys_scrap";
             if (this.name == newName) return;
             this.name = newName;
+
+            // NETWORK --
+            NetworkObject net = this.NetworkObject;
+            if (net)
+            {
+                FieldInfo ownershipField = typeof(NetworkObject).GetField("Ownership", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (ownershipField != null) ownershipField.SetValue(net, NetworkObject.OwnershipStatus.Transferable);
+
+                net.SynchronizeTransform = true;
+                net.SceneMigrationSynchronization = false;
+                net.ActiveSceneSynchronization = false;
+                net.SpawnWithObservers = true;
+                net.DontDestroyWithOwner = true;
+
+                net.AllowOwnerToParent = true;
+                net.AutoObjectParentSync = true;
+                net.SyncOwnerTransformWhenParented = true;
+            }
+
+            // ----------
+
+            if (!this._networkTransform) this._networkTransform = this.GetComponent<NetworkTransform>();
+            if (this._networkTransform)
+            {
+                this._networkTransform.SyncScaleX = false;
+                this._networkTransform.SyncScaleY = false;
+                this._networkTransform.SyncScaleZ = false;
+
+                this._networkTransform.AuthorityMode = NetworkTransform.AuthorityModes.Owner;
+                this._networkTransform.UseUnreliableDeltas = false;
+                this._networkTransform.UseHalfFloatPrecision = true;
+
+                this._networkTransform.Interpolate = true;
+                this._networkTransform.PositionThreshold = 0.001F;
+                this._networkTransform.PositionInterpolationType = NetworkTransform.InterpolationTypes.SmoothDampening;
+                this._networkTransform.PositionMaxInterpolationTime = 0.55F;
+
+                this._networkTransform.RotationInterpolationType = NetworkTransform.InterpolationTypes.SmoothDampening;
+                this._networkTransform.RotationMaxInterpolationTime = 0.35F;
+            }
+
+            if (!this._rigidbody) this._rigidbody = this.GetComponent<Rigidbody>();
+            if (this._rigidbody)
+            {
+                //this._rigidbody.interpolation = RigidbodyInterpolation.Interpolate; // Crashes editor
+                this._rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+                this._rigidbody.maxLinearVelocity = 10;
+                this._rigidbody.maxAngularVelocity = 10;
+                this._rigidbody.useGravity = true;
+                this._rigidbody.mass = 10;
+                this._rigidbody.linearDamping = 1;
+                this._rigidbody.angularDamping = 0.25F;
+            }
         }
         #endif
 
--- com.failcake.db.sdk/Scripts/Entities/entity_sdk/entity_sdk_interior_door.cs
+++ com.failcake.db.sdk/Scripts/Entities/entity_sdk/entity_sdk_interior_door.cs
@@ -1,7 +1,10 @@
 #region
 
 using System.Collections.Generic;
+using System.Reflection;
 using SaintsField.Playa;
+using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
 
 #endregion
@@ -11,8 +14,8 @@ namespace HyenaQuest
     /// <summary>
     ///     PROXY to entity_door_phys
     /// </summary>
-    [DisallowMultipleComponent]
-    public class entity_sdk_interior_door : MonoBehaviour
+    [DisallowMultipleComponent, RequireComponent(typeof(NetworkObject), typeof(NetworkTransform)), RequireComponent(typeof(NetworkRigidbody), typeof(Rigidbody))]
+    public class entity_sdk_interior_door : NetworkBehaviour
     {
         [LayoutStart("Door"), LayoutStart("Door/Layers", ELayout.Background | ELayout.TitleOut)]
         public List<GameObject> layers = new List<GameObject>();
@@ -40,12 +43,73 @@ namespace HyenaQuest
         }
 
         #if UNITY_EDITOR
+         /// <summary>
+        /// USED TO MIMIC entity_door
+        /// </summary>
+        
+        private NetworkTransform _networkTransform;
+        private Rigidbody _rigidbody;
+        
         public void OnValidate() {
             if (Application.isPlaying) return;
-
+            
             string newName = "entity_sdk_interior_door";
             if (this.name == newName) return;
             this.name = newName;
+
+            // NETWORK --
+            NetworkObject net = this.NetworkObject;
+            if (net)
+            {
+                FieldInfo ownershipField = typeof(NetworkObject).GetField("Ownership", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (ownershipField != null) ownershipField.SetValue(net, NetworkObject.OwnershipStatus.Transferable);
+
+                net.SynchronizeTransform = true;
+                net.SceneMigrationSynchronization = false;
+                net.ActiveSceneSynchronization = false;
+                net.SpawnWithObservers = true;
+                net.DontDestroyWithOwner = true;
+
+                net.AllowOwnerToParent = true;
+                net.AutoObjectParentSync = true;
+                net.SyncOwnerTransformWhenParented = true;
+            }
+
+            // ----------
+
+            if (!this._networkTransform) this._networkTransform = this.GetComponent<NetworkTransform>();
+            if (this._networkTransform)
+            {
+                this._networkTransform.SyncScaleX = false;
+                this._networkTransform.SyncScaleY = false;
+                this._networkTransform.SyncScaleZ = false;
+
+                this._networkTransform.AuthorityMode = NetworkTransform.AuthorityModes.Owner;
+                this._networkTransform.UseUnreliableDeltas = false;
+                this._networkTransform.UseHalfFloatPrecision = true;
+
+                this._networkTransform.Interpolate = true;
+                this._networkTransform.PositionThreshold = 0.001F;
+                this._networkTransform.PositionInterpolationType = NetworkTransform.InterpolationTypes.SmoothDampening;
+                this._networkTransform.PositionMaxInterpolationTime = 0.55F;
+
+                this._networkTransform.RotationInterpolationType = NetworkTransform.InterpolationTypes.SmoothDampening;
+                this._networkTransform.RotationMaxInterpolationTime = 0.35F;
+            }
+
+            if (!this._rigidbody) this._rigidbody = this.GetComponent<Rigidbody>();
+            if (this._rigidbody)
+            {
+                //this._rigidbody.interpolation = RigidbodyInterpolation.Interpolate; // Crashes editor
+                this._rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+                this._rigidbody.maxLinearVelocity = 10;
+                this._rigidbody.maxAngularVelocity = 10;
+                this._rigidbody.useGravity = true;
+                this._rigidbody.mass = 10;
+                this._rigidbody.linearDamping = 1;
+                this._rigidbody.angularDamping = 0.25F;
+            }
         }
         #endif
     }
