36565ef1daf34287b4efe035e8898a085766326a
FailCake <4944278+edunad@users.noreply.github.com>
2026-03-20 21:47:03 +0100

    Add round range

--- com.failcake.db.sdk/Scripts/Entities/entity_network_spawn/entity_network_template_round.cs
+++ com.failcake.db.sdk/Scripts/Entities/entity_network_spawn/entity_network_template_round.cs
@@ -10,11 +10,11 @@ namespace HyenaQuest
 {
     public class entity_network_template_round : entity_network_template_chance
     {
-        [LayoutStart("Settings", ELayout.Background | ELayout.TitleOut)]
-        public int minRounds;
+        [LayoutStart("Settings", ELayout.Background | ELayout.TitleOut), Range(1, byte.MaxValue)]
+        public byte minRounds;
         
         public override bool CanSpawn() {
-            byte currentRound = SDK_SETUP.GetCurrentRound?.Invoke() ?? 0;
+            byte currentRound = SDK_SETUP.GetCurrentRound?.Invoke() ?? 1;
             if (currentRound < this.minRounds) return false;
 
             return Random.Range(0f, 1f) < this.chance;
